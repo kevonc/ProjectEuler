ae1f3fd055b38f7844406b05f600b764d290483e
Kevon Cheung <kevon.cheung@gmail.com>
2013-08-06 08:02:35 -0400

    Solved #7

---
+++ 007_10001st_prime.rb
@@ -0,0 +1,15 @@
+number = 3
+devisor = 2
+prime = [2]
+
+while prime.length < 10001
+  while number % devisor != 0
+    devisor -= 1
+    prime << number if devisor == 1
+  end
+  number += 1
+  devisor = number - 1
+end
+
+puts prime.length
+puts prime[10000]
